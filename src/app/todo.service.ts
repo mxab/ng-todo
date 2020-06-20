640334af916ffab5b58fad2adfc1853d8ba952f0
Max Fröhlich <maxbruchmann@gmail.com>
2020-06-20 18:19:24 +0200

    basic setup

---
+++ src/app/todo.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {Todo} from './todo';
+import {delay} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+
+  db: Todo[] = [
+    {
+      name: 'Buy milk',
+      done: true
+    },
+    {
+      name: 'Bake cake',
+      done: false
+    }
+  ];
+
+  constructor() {
+  }
+
+  public findAll(): Observable<Todo[]> {
+    return of(this.db).pipe(delay(2000));
+  }
+}
