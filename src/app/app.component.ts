5edcf2d2e5e755f4d941c82f60900918463cb893
Max Fröhlich <maxbruchmann@gmail.com>
2020-06-20 22:07:57 +0200

    catch error and display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { TodoService } from './todo.service';
+import { catchError, map } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+import { Todo } from './todo';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +12,23 @@ import { TodoService } from './todo.service';
 export class AppComponent {
   title = 'ng-todo';
 
-  todoState$ = this.todoService.findAll();
+  todoState$: Observable<{
+    todos: Todo[];
+    error?: string;
+  }> = this.todoService.findAll().pipe(
+    map((todos) => {
+      return {
+        todos,
+        error: null,
+      };
+    }),
+    catchError((e) => {
+      return of({
+        todos: [],
+        error: e.toString(),
+      });
+    })
+  );
 
   constructor(private todoService: TodoService) {}
 }
